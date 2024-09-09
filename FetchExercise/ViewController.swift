4866487c571a4a8fb083bee349c210db4514c082
r.mironyuk <mironyuk.roma@gmail.com>
2024-09-08 22:15:28 -0400

    Initial Commit

---
+++ FetchExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FetchExercise
+//
+//  Created by Roman Mironyuk on 08.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
